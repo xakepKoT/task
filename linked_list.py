93f78d5345165903914e4aad82b24a7f892973e3
xakepKoT <sashamochalin2007@gmail.com>
2026-04-29 15:38:11 +0700

    first try

--- linked_list.py
+++ linked_list.py
@@ -32,3 +32,51 @@ def prepend(el: slist, new):
     l = slist()
     l.data = new
     l.next = el
+
+def get_last(el:slist):
+    while el.next != None:
+        el = el.next
+    return el.data
+
+def find(el:slist, needle):
+    ind = 0
+    while el.data != needle and el!=None:
+        el = el.next
+    if el.data == needle:
+        return ind
+    else:
+        return -1
+
+def remove_first(el:slist, needle):
+    start = el
+    old = None
+    while el.data != needle and el!=None:
+        old = el
+        el = el.next
+    if el.data == needle:
+        old.next = el.next
+    return start
+
+def remove_all(el:slist, needle):
+    start = el
+    old = None
+    while el != None:
+        old = el
+        el = el.next
+        if el.data == needle:
+            old.next = el.next
+    return start
+
+def copy(el: slist):
+    start = slist()
+    start.data = el.data
+    start.next = el.next
+    while el != None:
+        el=el.next
+        middle = slist()
+        middle.data = el.data
+        middle.next=el.next
+    return start
+
+def concat(el1:slist, el2:slist):
+    pass
